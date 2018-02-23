77feb00bb21b33be2d8ceb9ef622868514f27b79
Chan Jun Wei <chanjunweimy@gmail.com>
2018-02-23 14:32:52 +0800

    added a console application to test

--- EngagementRatioAutomation/Controllers/TfsTaskController.cs
+++ EngagementRatioAutomation/Controllers/TfsTaskController.cs
@@ -1,6 +1,11 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.WebApi;
 
 namespace EngagementRatioAutomation.Controllers
 {
@@ -18,6 +23,18 @@ namespace EngagementRatioAutomation.Controllers
         public IActionResult GetWorkItems()
         {
             var user = (WindowsIdentity) User.Identity;
+            var wiql =
+                "SELECT [System.Id], [System.WorkItemType], [Microsoft.VSTS.Common.Activity], [System.Title], [System.AssignedTo], [System.State], [System.IterationPath], [Microsoft.VSTS.Common.ClosedDate], [System.AreaPath] FROM WorkItems WHERE[System.TeamProject] = @project and[System.AssignedTo] = @me ORDER BY[System.ChangedDate] DESC";
+            //var wiql =
+            //    "SELECT [System.Id], [System.WorkItemType], [Microsoft.VSTS.Common.Activity], [System.Title], [System.AssignedTo], [System.State], [System.IterationPath], [Microsoft.VSTS.Common.ClosedDate], [System.AreaPath] FROM WorkItemLinks WHERE (Source.[System.TeamProject] = @project and Source.[System.State] <> 'Removed' and Source.[System.WorkItemType] <> 'Task') and ([System.Links.LinkType] = 'System.LinkTypes.Hierarchy-Forward') and (Target.[System.TeamProject] = @project and Target.[System.State] = 'Done' and Target.[Microsoft.VSTS.Common.ClosedDate] <= '2018-12-31T00:00:00.0000000' and Target.[Microsoft.VSTS.Common.ClosedDate] >= '2018-01-01T00:00:00.0000000' and Target.[System.AssignedTo] = 'Chan Jun Wei <NUMTECH\\jw.chan>' and Target.[System.WorkItemType] = 'Task') ORDER BY [System.Id] mode(Recursive,ReturnMatchingChildren)";
+            
+            // Create instance of VssConnection using Windows credentials (NTLM)
+            //VssConnection connection = new VssConnection(new Uri(collectionUri), new VssClientCredentials());
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            //WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+            //List<QueryHierarchyItem> items = witClient.GetQueriesAsync(teamProjectName).Result;
+
             return new ObjectResult(user.Name);
         }
     }
---
+++ TfsClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Principal;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace TfsClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = WindowsIdentity.GetCurrent();
+
+            var collectionUri = "http://aws-tfs:8080/tfs/NtCloud";
+            var teamProjectName = "NtCloud";
+            // Create instance of VssConnection using Windows credentials (NTLM)
+            VssConnection connection = new VssConnection(new Uri(collectionUri), new VssCredentials(new WindowsCredential(true)));
+            
+            //create a wiql object and build our query
+            Wiql wiql = new Wiql()
+            {
+                Query = "Select [System.Id], [System.WorkItemType], [Microsoft.VSTS.Common.Activity], [System.Title], [System.AssignedTo], [System.State], [System.IterationPath], [Microsoft.VSTS.Common.ClosedDate], [System.AreaPath] " +
+                        "From WorkItemLinks " +
+                        "Where (Source.[System.TeamProject] = '" + teamProjectName + "' and Source.[System.State] <> 'Removed' and Source.[System.WorkItemType] <> 'Task') " +
+                        "And ([System.Links.LinkType] = 'System.LinkTypes.Hierarchy-Forward') " +
+                        "And (Target.[System.TeamProject] = '" + teamProjectName + "' and Target.[System.State] = 'Done' and Target.[Microsoft.VSTS.Common.ClosedDate] <= '2018-12-31T00:00:00.0000000' and Target.[Microsoft.VSTS.Common.ClosedDate] >= '2018-01-01T00:00:00.0000000' and Target.[System.AssignedTo] = '" + user.Name + "' and Target.[System.WorkItemType] = 'Task') " +
+                        "Order By [System.Id] mode (Recursive, ReturnMatchingChildren) "
+            };
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+            WorkItemQueryResult workItemQueryResult = witClient.QueryByWiqlAsync(wiql).Result;
+
+        }
+    }
+}
