9f192b092f186e60025dfcc10affb7fc4bd75358
Chan Jun Wei <junwei.chan@numtech.com>
2018-03-12 17:17:53 +0800

    Change UI Name and added README.md

--- CommitmentReport.Ui/src/app/app.component.ts
+++ CommitmentReport.Ui/src/app/app.component.ts
@@ -7,7 +7,7 @@ import * as $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Engagement Ratio UI';
+  title = 'Commitment Report';
 
   constructor () {
   }
---
+++ README.md
@@ -0,0 +1,2 @@
+# CommitmentReport
+![Build-Status](http://aws-tfs:8080/tfs/NumtechSg/_apis/public/build/definitions/6b7f850b-1350-483e-a48d-733099e5999c/1/badge)
